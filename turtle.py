9ad1aa7876a7288df9c3a655484a9a6a8be0c329
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2025-11-16 13:31:34 -0500

    add demo

--- turtle.py
+++ turtle.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
+import turtle as t
 
-print("hello, cossac!")
-
-# this is a stub. continue from here!
+# this makes a pretty star!
+t.color("red")
+t.fillcolor("yellow")
+t.begin_fill()
+while True:
+    t.forward(200)
+    t.left(170)
+    if abs(t.pos()) < 1:
+        break
+t.end_fill()
+t.mainloop()
